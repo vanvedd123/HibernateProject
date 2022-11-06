e9ac8b020522b5495e75ea754620adaf8465f383
vanvedd123 <117584535+vanvedd123@users.noreply.github.com>
2022-11-06 21:37:43 +0530

    Hibernate

---
+++ src/OperationService/OperationService.java
@@ -0,0 +1,22 @@
+package OperationService;
+
+public interface OperationService {
+	
+void addUserDataWithMultipleProduct();
+	
+	void showAllUserWithMultipleProductData();
+	
+	void getSingleUserWithMultipleProductData();
+	
+	void updateUser();
+	
+	void updatePerticularProductDataUsingUserId();
+	
+	void deletePerticularProductDataUsingUserId();
+	
+	void deleteUser();
+	
+	void addProductDataToExistingUser(); 
+}
+
+
